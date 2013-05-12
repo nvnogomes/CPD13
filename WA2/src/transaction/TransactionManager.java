f53b596b5ccea5c56e488c3fa44ec8d0499dfa8f
Serhiy Moskovchuk <serhiy.mos@mail.ru>
2013-05-12 17:12:04 +0100

    TransactionManager impelementetion BETA
    duvida: não existe variavel status, ou seja para saber se a transacção está correr/parada/comiited…
    é preciso adiciona.la ?

--- WA2/src/transaction/TransactionManager.java
+++ WA2/src/transaction/TransactionManager.java
@@ -1,7 +1,14 @@
 package transaction;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+
+import locker.LockServer;
+import util.Status;
+import util.Status.Result;
+import bank.AccountManagement;
+import bank.IBankServer;
 
 /**
  * Transaction Manager implementation
@@ -28,6 +35,8 @@ public class TransactionManager implements ITransactionServer {
 	 * 
 	 */
 	protected Map<Integer,Transaction> log;
+    protected LockServer ls;
+    protected IBankServer acm;
 	
 
 	/**
@@ -47,6 +56,13 @@ public class TransactionManager implements ITransactionServer {
 	private TransactionManager() {
 		log = new HashMap<Integer,Transaction>();
 		currentId = 0;
+
+        //lockServer
+        ls = LockServer.getInstance();
+        
+        //AccountManager
+        acm = AccountManagement.getInstance();
+
 	}
 	
 	/**
@@ -71,49 +87,108 @@ public class TransactionManager implements ITransactionServer {
 	public int beginT() {
 		int id = getNextId();
 		log.put(id, new Transaction());
-		
+
 		return id;
 	}
 
 	@Override
 	public int commitT(int transactionId) {
-		// TODO
+		//TODO
+
+		Transaction t = log.get(transactionId);
+		Iterator<TransactLog> it = t.getIterator();
 		
-		// schedule events?
-		return 0;
+		TransactLog tl = null;
+		Result result = Status.Result.OK;
+		
+		while(it.hasNext()){
+			tl = it.next();
+			
+			if(tl.isWriteOperation()){
+				result = ls.unlockWrite(tl.accountId).getStat();
+			}else{
+				result = ls.unlockRead(tl.accountId).getStat();
+			}
+			
+			if(result != Result.OK){
+				//algum unlock falhado
+				return -1;
+			}
+		}
+		
+		return 1;
 	}
 
 	@Override
 	public int abortT(int transactionId) {
 		// TODO
 		
+		Transaction t = log.get(transactionId);
+		Iterator<TransactLog> it = t.getIterator();
+		
+		TransactLog tl = null;
+		Result result = Status.Result.OK;
+		
+		while(it.hasNext()){
+			tl = it.next();
+			
+			if(tl.isWriteOperation()){
+				result = acm.writeAccount(tl.accountId, tl.oldValue).getStat();
+			}
+			
+			if(result != Result.OK){
+				//algum write falhado
+				return -1;
+			}
+		}
+		
+		while(it.hasNext()){
+			tl = it.next();
+			
+			if(tl.isWriteOperation()){
+				result = ls.unlockWrite(tl.accountId).getStat();
+			}
+			
+//			else{
+//				result = ls.unlockRead(tl.accountId).getStat();
+//			}
+			
+			if(result != Result.OK){
+				//algum unlock falhado
+				return -1;
+			}
+		}
+		
+		
+		
 		// restore old values
-		return 0;
+		return 1;
 	}
 
 	@Override
 	public int readT(int accontNum) {
 		// TODO
 		
-		// get Lock
-		
-		
-		// log event
-		log.get(currentId).addRead(accontNum);
+        if(ls.lockRead(accontNum).getStat() == Result.OK){
+    		log.get(currentId).addRead(accontNum);
+    		
+    		return 1;
+        }
 		return 0;
 	}
 
 	@Override
 	public int writeT(int accontNum, int balance) {
 		// TODO
-		// get old value
+		
 		int oldVal = 0;
-		
-		// get write lock
-		
-		// log event
-		log.get(currentId).addWrite(accontNum, oldVal, balance);
-		
+		if(ls.lockWrite(accontNum).getStat() == Result.OK){
+			oldVal = acm.readAccount(accontNum).getValue();
+			
+			log.get(currentId).addWrite(accontNum, oldVal, balance);
+			
+			return 1;
+		}
 		return 0;
 	}
 
